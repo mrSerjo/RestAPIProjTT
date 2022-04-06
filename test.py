b3209f89b3f85e0f254a942eddd1482e7530d09e
mrserjo <mrserjo@mail.ru>
2022-04-06 15:05:30 +0300

    hw

--- test.py
+++ test.py
@@ -3,5 +3,8 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-response = requests.put(BASE + 'video/1', {'likes': 10})
+# response = requests.put(BASE + 'video/1', {'likes': 10, 'name': 'Tim', 'views': 10000, })
+# print(response.json())
+
+response = requests.put(BASE + 'video/6')
 print(response.json())
