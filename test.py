e33962dedc719360853bffdd6aa07e12ee1dd4dc
mrserjo <mrserjo@mail.ru>
2022-04-07 10:13:09 +0300

    put reworked, patch added

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-# response = requests.put(BASE + 'video/1', {'likes': 10, 'name': 'Tim', 'views': 10000, })
-# print(response.json())
 
-response = requests.put(BASE + 'video/6')
+response = requests.patch(BASE + 'video/2', {'views': 99})
 print(response.json())
